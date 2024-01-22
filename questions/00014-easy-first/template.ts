39751fddb8d3779eb278c73737211fc7d10350b6
Dinosaur <442283045@qq.com>
2024-01-22 14:48:45 +0800

    14

--- questions/00014-easy-first/template.ts
+++ questions/00014-easy-first/template.ts
@@ -1,1 +1,8 @@
-type First<T extends any[]> = any
+type First<T extends any[]> = T extends [infer R, ...any] ? R : never
+
+// Other answers
+// answer1
+type First1<T extends any[]> = T extends [] ? never : T[0]
+
+// answer2
+type First2<T extends any[]> = T['length'] extends 0 ? never : T[0]
