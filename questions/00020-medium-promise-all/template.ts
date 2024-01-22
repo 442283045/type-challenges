0035ab27bdf29ea81f926aea8c9eb3b8d8b3ce4b
Dinosaur <442283045@qq.com>
2024-01-22 17:14:47 +0800

    20

--- questions/00020-medium-promise-all/template.ts
+++ questions/00020-medium-promise-all/template.ts
@@ -1,1 +1,7 @@
-declare function PromiseAll(values: any): any
+type AwaitedTest<T> = T extends Promise<infer R> ? AwaitedTest<R> : T
+
+declare function PromiseAll<T extends any[]>(
+  values: readonly [...T]
+): Promise<{
+  [P in keyof T]: AwaitedTest<T[P]>
+}>
