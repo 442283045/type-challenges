a402e90bff9875414233ec4b96cf41aeaf687f88
Dinosaur <442283045@qq.com>
2024-01-28 17:11:43 +0800

    new solution

--- questions/00062-medium-type-lookup/template.ts
+++ questions/00062-medium-type-lookup/template.ts
@@ -1,1 +1,7 @@
-type LookUp<U, T> = any
+type LookUp<U, T> = U extends { type: T } ? U : never
+
+// type LookUp<U extends { type: any }, T> = U extends { type: infer R }
+//   ? R extends T
+//     ? U
+//     : never
+//   : never
