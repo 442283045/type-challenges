46e2decaee05b5148e5f233e6bab6ebb9f1e0953
Dinosaur <442283045@qq.com>
2024-02-15 22:48:52 +0800

    new solution

--- questions/00112-hard-capitalizewords/template.ts
+++ questions/00112-hard-capitalizewords/template.ts
@@ -1,1 +1,6 @@
-type CapitalizeWords<S extends string> = any
+type CapitalizeRest<S extends string> = S extends `${infer F}${infer R}`
+  ? `${F}${CapitalizeRest<
+      Uppercase<F> extends Lowercase<F> ? Capitalize<R> : R
+    >}`
+  : S
+type CapitalizeWords<S extends string> = Capitalize<CapitalizeRest<S>>
